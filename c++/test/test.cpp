da93903425a440c39264462fd23ccd2f848a30ac
koll <zxb.1223@163.com>
2020-03-08 16:31:02 +0800

    string stream

---
+++ c++/test/test.cpp
@@ -0,0 +1,15 @@
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <iostream>
+using namespace std;
+int main(int argc,char* argv[])
+{
+    ifstream fs;
+    fs.open("test.cpp");
+    stringstream buf;
+    buf << fs.rdbuf();
+    std::string content = buf.str();
+    cout << content.c_str() << "\n";
+    return 0;
+}
