919800e0b9011d77c6a2a9ec4115f5c328475198
koll <zxb.1223@163.com>
2022-08-04 11:39:14 +0800

    CMAKE: generate binary name

--- c++/cmake/common.cmake
+++ c++/cmake/common.cmake
@@ -12,3 +12,10 @@ function(test_func_args)
 	set(multiArgs PUBLIC PRIVATE) #args those take multiple value
   cmake_parse_arguments(test_arg "${options}" "${oneValueArgs}" "${multiArgs}" ${ARGN})
 endfunction()
+
+function(generate_bin_name bin_name)
+	string(FIND  ${CMAKE_CURRENT_SOURCE_DIR} "/" POS REVERSE)
+	math(EXPR POS "${POS}+1")
+	string(SUBSTRING ${CMAKE_CURRENT_SOURCE_DIR} ${POS} -1 DIR_NAME)
+	set(${bin_name} ${DIR_NAME} PARENT_SCOPE)
+endfunction()
