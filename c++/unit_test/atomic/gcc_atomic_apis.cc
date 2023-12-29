64734e2f4a874e0d356fddd4594a57b986238632
koll <zxb.1223@163.com>
2023-12-29 17:29:36 +0800

    cpp: memory reordering

--- c++/unit_test/atomic/gcc_atomic_apis.cc
+++ c++/unit_test/atomic/gcc_atomic_apis.cc
@@ -10,7 +10,42 @@ using namespace std;
 //to the functions. New code should always use the ‘__atomic’ builtins 
 //rather than the ‘__sync’ builtins
 
-TEST(TESTNAME, atomic_fetch_add_test) {
+/* memory_order_relaxed：
+    there are no synchronization or ordering constraints imposed on other reads or writes,
+    only this operation's atomicity is guaranteed (see Relaxed ordering below).
+
+  memory_order_consume
+    A load operation with this memory order performs a consume operation
+    on the affected memory location: no reads or writes in the current thread 
+    dependent on the value currently loaded can be reordered before this load.
+    Writes to data - dependent variables in other threads that release the same atomic
+    variable are visible in the current thread.On most platforms, this affects compiler
+    optimizations only
+
+  memory_order_acquire
+    A load operation with this memory order performs the acquire operation on the affected memory location:
+    no reads or writes in the current thread can be reordered before this load .
+    All writes in other threads that release the same atomic variable are visible in the current thread
+            
+  memory_order_release A store operation with
+    this memory order performs the release operation : no reads or writes in the current thread can be reordered after
+    this store.All writes in the current thread are visible in other threads that acquire the same atomic
+    variable and writes that carry a dependency into the atomic variable become visible in other threads that consume the same
+    atomic
+                    
+  memory_order_acq_rel
+    A read - modify - write operation with this memory order is both an acquire
+    operation and a release operation.No memory reads or writes in the current thread can be reordered before the load,
+    nor after the store All writes in other threads that release the same atomic variable are visible before the modification and the
+    modification is visible in other threads that acquire the same atomic variable
+    
+  memory_order_seq_cst
+    A load operation with this memory order performs an acquire operation, a store performs a release operation,
+    and read - modify - write performs both an acquire operation and a release operation,
+    plus a single total order exists in which all threads observe all
+    modifications in the same order */
+
+    TEST(TESTNAME, atomic_fetch_add_test) {
   int stats = 0;
 
   auto stats_increment = [&stats](int n) {
